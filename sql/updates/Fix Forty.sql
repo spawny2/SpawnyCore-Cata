fa9af68e7522bb3ec6176cf5227488a4b81beff0
Hristo Lungov <itseto@abv.bg>
2011-06-09 09:09:08 +0300

    Fix commit

--- sql/updates/Fix Forty.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
-(79104, -79105, 0, ''),
-(79104, -79105, 1, '');
-
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
-(79105, -79104, 0, ''),
-(79105, -79104, 1, '');
